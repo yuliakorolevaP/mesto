2b265410ba2777ff0cee63a79432a7f9423434d7
Yulia Korolyova <yukorolyova1@yandex.ru>
2023-03-19 00:56:40 +0300

    1 работа

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let buttompensil = document.querySelector('.profile__button_pensil');
 let element = document.querySelector('.popup');
-let elements = document.querySelector('#elements');
-let buttomelement = document.querySelector('.element__button');
 let buttomexit = document.querySelector('.popup__exit');
 let formElement = document.querySelector('.popup__form');
 let text = document.querySelector('.popup_input_type_text');
@@ -10,24 +8,22 @@ let title = document.querySelector('.profile__title');
 let subtitle=document.querySelector('.profile__subtitle');
 function showClick() {
   element.classList.add('popup_opened');
-}
-function showClick2() {
-  buttomelement.classList.add('element__buttom_active');
   }
-function showClick1() {
+
+function showClickexit() {
   element.classList.remove('popup_opened');
   }
+ 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  let textt = text.value;
-  let typee = type.value;
-  title.textContent=textt;
-  subtitle.textContent=typee;
-  showClick1();
+  let textvalue = text.value;
+  let typevalue = type.value;
+  title.textContent=textvalue;
+  subtitle.textContent=typevalue;
+  showClickexit();
   }
 text.value= title.textContent;
 type.value= subtitle.textContent;
-buttomexit.addEventListener('click', showClick1); 
+buttomexit.addEventListener('click', showClickexit); 
 buttompensil.addEventListener('click', showClick); 
-buttomelement.addEventListener('click', showClick2); 
 formElement.addEventListener('submit', handleFormSubmit);  
