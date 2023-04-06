24bbcfe0383bce37a601ce3e7b68ef5770a50078
Yulia Korolyova <yukorolyova1@yandex.ru>
2023-04-06 04:31:06 +0300

    Исправлено пр5

--- index.js
+++ index.js
@@ -24,21 +24,25 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ]; 
-let buttompensil = document.querySelector('.profile__button-pensil');
-let buttomexit = document.querySelector('.popup__exit');
-let formElement = document.querySelector('.popup__form');
-let text = document.querySelector('.popup__input_type_text');
-let type=document.querySelector('.popup__input_type_status');  
-let title = document.querySelector('.profile__title'); 
-let subtitle=document.querySelector('.profile__subtitle');
-let buttomplus = document.querySelector('.profile__button-plus');
+const buttompensil = document.querySelector('.profile__button-pensil');
+const buttomexit = document.querySelector('.popup__exit');
+const formElement = document.querySelector('.popup__form');
+const text = document.querySelector('.popup__input_type_text');
+const type=document.querySelector('.popup__input_type_status');  
+const title = document.querySelector('.profile__title'); 
+const subtitle=document.querySelector('.profile__subtitle');
+const buttomplus = document.querySelector('.profile__button-plus');
 const popupAddImage = document.querySelector('.popup_image');
-const popupUpdate = document.querySelector('.popup');
-let buttomexitAddImage = document.querySelector('.popup__exit_image');
-const AllImage=document.querySelector('.elements');
-let nameImage = document.querySelector('.popup__input_type_name');
-let srcImage=document.querySelector('.popup__input_type_src');
+const popupUpdate = document.querySelector('.popup_edit');
+const buttomexitAddImage = document.querySelector('.popup__exit_image');
+const nameImage = document.querySelector('.popup__input_type_name');
+const srcImage=document.querySelector('.popup__input_type_src');
 const formElementAdd = document.querySelector('.popup__form_image');
+const buttomexitZoom = document.querySelector('.popup__exit_zoom');
+const popupZoomImage=document.querySelector('.popup_zoom');
+const elements=document.querySelector('.elements');
+const titleZoom=document.querySelector('.popup__title-zoom');
+const popupImage=document.querySelector('.popup__image');
 const openPopup = function (popup) {
   popup.classList.add('popup_opened');
 }
@@ -74,12 +78,10 @@ formElement.addEventListener('submit', function(evt) {
   closePopup(popupUpdate);
 });
 
-function addImage(a) {
+function createCard(a) {
   const imageTemplate = document.querySelector('#element-template').content;
   const image = imageTemplate.querySelector('.element__item').cloneNode(true);
   const imageZoom=image.querySelector('.element__image');
-  const popupZoomImage=document.querySelector('.popup_zoom');
-  let buttomexitZoom = document.querySelector('.popup__exit_zoom');
   const detetebutton=image.querySelector('.element__button-delete');
   const likebutton=image.querySelector('.element__button');
   detetebutton.addEventListener('click', ()=>{
@@ -90,41 +92,31 @@ function addImage(a) {
   });
   imageZoom.addEventListener('click', function(){
     openPopup(popupZoomImage);
-    document.querySelector('.popup__title-zoom').textContent=a.name;
-    document.querySelector('.popup__image').alt=a.name;
-    document.querySelector('.popup__image').src=a.link;
-  }); 
-  buttomexitZoom.addEventListener('click', function(){
-    closePopup(popupZoomImage);
+    titleZoom.textContent=a.name;
+    popupImage.alt=a.name;
+    popupImage.src=a.link;
   }); 
   image.querySelector('.element__title').textContent=a.name;
   image.querySelector('.element__image').alt=a.name;
   image.querySelector('.element__image').src=a.link;
-  AllImage.append(image);
   return image;
 }
 
 initialCards.forEach(elem=>{
-  let newImage = addImage(elem);
-  document.querySelector('.elements').append(newImage);
+  let newImage = createCard(elem);
+  elements.append(newImage);
 });
 
 formElementAdd.addEventListener('submit', function (evt) {
   evt.preventDefault();
-  const newImage = addImage({
+  const newImage = createCard({
     name: nameImage.value,
     link: srcImage.value
   });
-  document.querySelector('.elements').prepend(newImage);
+  elements.prepend(newImage);
   closePopup(popupAddImage);
+  evt.target.reset();
 });  
-
-
-      
-
-
-
-
-  
-
-
+buttomexitZoom.addEventListener('click', function(){
+  closePopup(popupZoomImage);
+}); 
